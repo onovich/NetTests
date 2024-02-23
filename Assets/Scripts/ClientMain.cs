cca903d45acedd3c8b222012c0c88292486a4dae
onovich <hjn1110@163.com>
2024-02-23 22:27:43 +0800

    <refactor> modify: 使用 Socket 代替 TcpClient

--- Assets/Scripts/ClientMain.cs
+++ Assets/Scripts/ClientMain.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,22 +11,21 @@ public class ClientMain : MonoBehaviour {
 
     void ConnectToServer() {
         try {
-            System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient("127.0.0.1", 8080);
-            NetworkStream stream = client.GetStream();
+            // Create a TCP/IP socket.
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
 
             // Receive data from the server
             byte[] data = new byte[256];
-            int bytes = stream.Read(data, 0, data.Length);
+            int bytes = client.Receive(data);
             string responseData = Encoding.ASCII.GetString(data, 0, bytes);
             Debug.Log("Received: " + responseData);
 
-            // Close everything
-            stream.Close();
+            // Close the socket
+            client.Shutdown(SocketShutdown.Both);
             client.Close();
         } catch (SocketException e) {
             Debug.Log("SocketException: " + e.ToString());
         }
-
     }
-
 }
