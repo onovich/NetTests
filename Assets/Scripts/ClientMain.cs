5207c08cec33a12c945b1eee08d0f37b91350e90
onovich <hjn1110@163.com>
2024-03-18 17:39:07 +0800

    <refactor> modify: 使用 Rill 重构客户端

--- Assets/Scripts/ClientMain.cs
+++ Assets/Scripts/ClientMain.cs
@@ -6,6 +6,7 @@ using System.Text;
 using UnityEngine.UI;
 using MortiseFrame.LitIO;
 using System;
+using MortiseFrame.Rill;
 
 public class ClientMain : MonoBehaviour {
 
@@ -15,126 +16,78 @@ public class ClientMain : MonoBehaviour {
     [SerializeField] Button connectButton;
     [SerializeField] Button closeButton;
 
-    Queue<IMessage> messageQueue;
-
-    byte[] data;
-    Socket client;
+    ClientCore clientCore;
 
     void Start() {
-        data = new byte[256];
-        loginButton.onClick.AddListener(SendLoginReq);
-        connectButton.onClick.AddListener(ConnectToServer);
-        closeButton.onClick.AddListener(CloseConnection);
-        messageQueue = new Queue<IMessage>();
+
+        loginButton.onClick.AddListener(OnClickLogin);
+        connectButton.onClick.AddListener(OnClickConnect);
+        closeButton.onClick.AddListener(OnClickClose);
+
+        clientCore = new ClientCore();
+        Register();
+    }
+
+    void On() {
+        clientCore.OnConnect(OnConnect);
+        clientCore.On<ConnectResMessage>((msg) => OnConnectRes((ConnectResMessage)msg));
+        clientCore.On<LoginResMessage>((msg) => LoginRes((LoginResMessage)msg));
+    }
+
+    void LoginRes(LoginResMessage msg) {
+        Debug.Log("On LoginRes; status: " + msg.status + ", userToken: " + msg.userToken);
+        recvText.text = "LoginRes; status: " + msg.status + ", userToken: " + msg.userToken;
+    }
+
+    void OnConnectRes(ConnectResMessage msg) {
+        Debug.Log("On ConnectRes");
+        recvText.text = "ConnectRes";
+    }
+
+    void OnConnect() {
+        Debug.Log("On Connected");
+        recvText.text = "Connected";
+    }
+
+    void OnError(string error) {
+        Debug.Log("On Error: " + error);
+        recvText.text = "Error: " + error;
+    }
+
+    void Register() {
+        clientCore.Register(typeof(ConnectResMessage));
+        clientCore.Register(typeof(LoginResMessage));
+        clientCore.Register(typeof(LoginReqMessage));
+        clientCore.Register(typeof(CloseReqMessage));
+    }
+
+    void OnClickConnect() {
+        clientCore.Connect("127.0.0.1", 8080);
+    }
+
+    void OnClickLogin() {
+        LoginReqMessage msg = new LoginReqMessage();
+        msg.userToken = userName.text;
+        clientCore.Send(msg);
+    }
+
+    void OnClickClose() {
+        CloseReqMessage msg = new CloseReqMessage();
+        clientCore.Send(msg);
     }
 
     void Update() {
-        if (client == null) {
+        if (clientCore == null) {
             return;
         }
-        TickOn();
-        TickSend();
-    }
-
-    void TickOn() {
-        if (client.Poll(0, SelectMode.SelectRead)) {
-            byte[] data = new byte[1024];
-            int count = client.Receive(data);
-            int offset = 0;
-            int msgCount = ByteReader.Read<int>(data, ref offset);
-
-            for (int i = 0; i < msgCount; i++) {
-                int len = ByteReader.Read<int>(data, ref offset);
-                if (len < 5) {
-                    break;
-                }
-                byte id = ByteReader.Read<byte>(data, ref offset);
-                Debug.Log("Recv Message ID: " + id + ", Length: " + len);
-                On(id, data, ref offset);
-            }
-        }
-    }
-
-    void TickSend() {
-        byte[] data = new byte[1024];
-        int offset = 0;
-        int msgCount = messageQueue.Count;
-        ByteWriter.Write<int>(data, msgCount, ref offset);
-        while (messageQueue.TryDequeue(out IMessage message)) {
-            byte[] src = message.ToBytes();
-            int len = src.Length + 5;
-            byte id = ProtocolDict.GetID(message);
-
-            ByteWriter.Write<int>(data, len, ref offset);
-            ByteWriter.Write<byte>(data, id, ref offset);
-            // ByteWriter.WriteArray<byte>(data, src, ref offset);
-            Buffer.BlockCopy(data, 0, this.data, 0, offset);
-            offset += src.Length;
-        }
-        if (offset > 0) {
-            client.Send(data, 0, offset, SocketFlags.None);
-        }
-    }
-
-    void On(int id, byte[] data, ref int offset) {
-        switch (id) {
-            case 102:
-                Debug.Log("On ConnectResMessage");
-                OnConnectRes(data, ref offset);
-                break;
-            case 106:
-                Debug.Log("On LoginResMessage");
-                OnLoginRes(data, ref offset);
-                break;
-            default:
-                Debug.LogError("Unknown Message ID: " + id);
-                break;
-        }
-    }
-
-    void SendLoginReq() {
-        LoginReqMessage loginReq = new LoginReqMessage();
-        loginReq.userToken = userName.text;
-
-        messageQueue.Enqueue(loginReq);
-    }
-
-    void OnConnectRes(byte[] data, ref int offset) {
-        recvText.text = "Connected To Server Success!";
-    }
-
-    void OnLoginRes(byte[] data, ref int offset) {
-        LoginResMessage loginRes = new LoginResMessage();
-        loginRes.FromBytes(data, ref offset);
-        recvText.text = "STATUS: " + loginRes.status + ", TOKEN: " + loginRes.userToken;
-    }
-
-    void ConnectToServer() {
-        try {
-            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
-        } catch (SocketException e) {
-            Debug.Log("SocketException: " + e.ToString());
-        }
-    }
-
-    void SendCloseReq() {
-        CloseReqMessage closeReq = new CloseReqMessage();
-        messageQueue.Enqueue(closeReq);
-    }
-
-    void CloseConnection() {
-        SendCloseReq();
-        client.Shutdown(SocketShutdown.Both);
-        client.Close();
-        recvText.text = "Connection Closed!";
+        clientCore.Tick(0);
     }
 
     void OnDestroy() {
-        CloseConnection();
-        loginButton.onClick.RemoveListener(SendLoginReq);
-        connectButton.onClick.RemoveListener(ConnectToServer);
-        closeButton.onClick.RemoveListener(CloseConnection);
+        if (clientCore == null) {
+            return;
+        }
+        clientCore.Stop();
     }
 
 }
